3c49df45c77f436b2ee12340579bbcdb2f65499a
Kithinga <menscfesz@gmail.com>
2022-03-01 03:01:17 +0300

    yes

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="head-ki"><h3>VIEWTODO</h3></div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family:Poppins;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.head-ki{
+  padding:2rem;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+     <h5>This is an about page</h5>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elite dicta?</p>
+      
+  </div>
+</template>
+
+
+<style scoped>
+      p{
+        font-size: 13px;
+        font-weight:400;
+      }
+</style>
