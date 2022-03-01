de270079c03bf7bd0bd07ce3f42b828da4663d27
Kithinga <menscfesz@gmail.com>
2022-03-01 03:37:12 +0300

    commited

--- src/App.vue
+++ src/App.vue
@@ -7,29 +7,11 @@
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family:Poppins;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-.head-ki{
-  padding:2rem;
-}
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+
+
+
+
