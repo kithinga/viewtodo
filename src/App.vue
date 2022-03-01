ca31aa9189e85864e72a0b836b454bf4a9ded366
Kithinga <menscfesz@gmail.com>
2022-03-01 04:10:44 +0300

    serious

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="head-ki"><h3>VIEWTODO</h3></div>
+  <div class="head-ki"><h2>Viewdo</h2></div>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
